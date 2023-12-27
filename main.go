0cdefd3b0e7f2f12a82cc11924eab84a7ca4ae3e
DEARn6sx <nantawats.study@gmail.com>
2023-12-27 15:47:27 +0700

    Variable

--- main.go
+++ main.go
@@ -8,7 +8,19 @@ import (
 )
 
 func main() {
-	ep1()
+	ep2()
+}
+
+func ep2() {
+	var fullname string = "DEARZA"
+	var age = 26
+	address := "Roiet"
+	fmt.Println(fullname, age)
+	fmt.Printf("Hello %s Yaaaay my age is %d myaddress is %s\n",
+		fullname, age, address)
+	fmt.Printf("Type name is %T \nType age is %T \nType address is %T \n",
+		fullname, age, address)
+
 }
 
 func ep1() {
