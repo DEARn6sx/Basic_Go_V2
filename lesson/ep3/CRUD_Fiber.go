047ff5eb4c58863013447408e80338b023eadaa0
DEARn6sx <nantawats.study@gmail.com>
2024-01-04 17:34:23 +0700

    Uploadfile Fiber

--- lesson/ep3/CRUD_Fiber.go
+++ lesson/ep3/CRUD_Fiber.go
@@ -21,6 +21,7 @@ func Ep3_Book() {
 	app.Post("/books", createBook)
 	app.Put("/books/:id", updateBook)
 	app.Delete("/books/:id", deleteBook)
+	app.Post("/upload", uploadFile)
 
 	app.Listen(":8080")
 }
