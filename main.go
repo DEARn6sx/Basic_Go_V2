05c59ed3406d744bdeb54506b31958433525a4f1
DEARn6sx <nantawats.study@gmail.com>
2024-01-06 00:16:13 +0700

    Create data

--- main.go
+++ main.go
@@ -16,24 +16,49 @@ const (
 	password     = "mypassword"
 )
 
+// ประกาศ db เป็น global เพื่อให้ db เป็นตัวแทนในการพูดคุย
+var db *sql.DB
+
+type Product struct {
+	ID    int
+	Name  string
+	Price int
+}
+
 func main() {
 	// Connection string
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, username, password, databaseName)
 
-	db, err := sql.Open("postgres", psqlInfo)
+	sdb, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		// This will not be a connection error, but a DSN parse error or
 		// another initialization error.
 		log.Fatal(err)
 	}
 
+	db = sdb
+
 	err = db.Ping()
 	if err != nil {
 		log.Fatal(err)
-	}	
+	}
 
-	print("conntecion db:",db)
+	fmt.Println("conntecion sucess")
 
+	err = createProducts(&Product{Name: "GoProduct2", Price: 22555})
+	if err != nil {
+		log.Fatal(err)
+	}
+	print("Create Success")
+}
+
+func createProducts(product *Product) error {
+	_, err := db.Exec(
+		"INSERT INTO public.products(name, price)VALUES ($1, $2);",
+		product.Name,
+		product.Price,
+	)
+	return err
 }
