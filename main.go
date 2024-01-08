f07be99b3a83c92833b26d1c3927f02d4351e15b
DEARn6sx <nantawats.study@gmail.com>
2024-01-08 20:45:58 +0700

    Select Data

--- main.go
+++ main.go
@@ -9,58 +9,79 @@ import (
 )
 
 const (
-	host         = "localhost"
-	port         = 5432
-	databaseName = "mydatabase"
-	username     = "myuser"
-	password     = "mypassword"
+	host     = "localhost"  // or the Docker service name if running in another container
+	port     = 5432         // default PostgreSQL port
+	user     = "myuser"     // as defined in docker-compose.yml
+	password = "mypassword" // as defined in docker-compose.yml
+	dbname   = "mydatabase" // as defined in docker-compose.yml
 )
 
-// ประกาศ db เป็น global เพื่อให้ db เป็นตัวแทนในการพูดคุย
 var db *sql.DB
 
 type Product struct {
-	ID         int
-	Name       string
-	Price      int
+	ID    int
+	Name  string
+	Price int
 }
 
 func main() {
 	// Connection string
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
-		host, port, username, password, databaseName)
+		host, port, user, password, dbname)
 
+	// Open a connection
 	sdb, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
-		// This will not be a connection error, but a DSN parse error or
-		// another initialization error.
 		log.Fatal(err)
 	}
 
 	db = sdb
 
-	//check db ส่งสัญญาณมาหรือไม่
+	// Check the connection
 	err = db.Ping()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("conntecion sucess")
+	fmt.Println("Successfully connected!")
 
-	err = createProducts(&Product{Name: "6", Price: 6})
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Create Successful")
+	//insert
+
+	// err = createProduct(&Product{Name: "Go product", Price: 222})
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// fmt.Println("Successfully Created!")
+
+	//search
+
+	product, err := getProduct(2)
+	fmt.Println(product)
 }
 
-func createProducts(product *Product) error {
-	//Exec เป็นการ run คำสั่งที่ไม่ต้องการ return กลับมา แค่ต้องการ return ว่า error มั้ยแค่นั้น
+func createProduct(product *Product) error {
+
 	_, err := db.Exec(
-		"INSERT INTO public.products(name, price)VALUES ($1, $2);",
+		"INSERT INTO public.products( name, price)VALUES ($1, $2 );",
 		product.Name,
 		product.Price,
 	)
+
 	return err
+}
+
+func getProduct(id int) (Product, error)  {
+	var p Product
+	row := db.QueryRow("SELECT id, name, price FROM products WHERE id = $1",
+	id,
+	)
+	err := row.Scan(&p.ID,&p.Name, &p.Price)
+	if err != nil {
+		return Product{}, err
+	}
+
+	return p, nil
+
 }
